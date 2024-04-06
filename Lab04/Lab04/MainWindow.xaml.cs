dd47327fb90bb15ffa82801858dd4557c2233a9b
WVillanuevaA <102330558+WVillanuevaA@users.noreply.github.com>
2024-04-06 13:07:48 -0500

    Add files via upload

---
+++ Lab04/Lab04/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab04
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("Data Source=LAB1504-30\\SQLEXPRESS; Initial Catalog=NeptunoDB; User Id=Eduardo; Password=123456");
+            conexion.Open();
+
+            SqlCommand command = new SqlCommand();
+        }
+    } 
+}
